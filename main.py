5332b4d509773a0e780f0f8a9d8557fc08bbec0d
Yasir <DPSA00609@mypaceschool.com>
2023-10-30 16:58:37 +0400

    Making the backend and connecting it to the window

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from PyQt6.QtWidgets import (QMainWindow, QTextEdit, QLineEdit, QPushButton,
                              QApplication)
+import openai
 import sys
+import os
 
 
 class ChatBot:
-    pass
+    def __init__(self, user_input):
+        my_key = os.getenv("OPENAI_API_KEY")
+        openai.api_key = my_key
+        self.user_input = user_input
+    
+    def get_responce(self):
+        responce = openai.Completion.create(
+            engine="gpt-3.5-turbo",
+            prompt=self.user_input,
+            max_tokens=4080,
+            temperature=0.5
+        ).choices[0].text
+        return responce
 
 
 class ChatBotWindow(QMainWindow):
@@ -12,19 +26,26 @@ class ChatBotWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Chat Bot")
         self.setMinimumSize(700, 500)
-
         self.chat_area = QTextEdit(self)
         self.chat_area.setGeometry(10, 10, 480, 320)
         self.chat_area.setReadOnly(True)
-
         self.input_box = QLineEdit(self)
         self.input_box.setGeometry(10, 340, 480, 40)
-
-
         self.ok_button = QPushButton("Send", self)
         self.ok_button.setGeometry(500, 340, 100, 40)
-
+        self.ok_button.clicked.connect(self.send_message)
         self.show()
+    
+    def send_message(self):
+        user_input = self.input_box.text().strip()
+        self.chat_area.append(
+            f"<p style='color:#333333'>Me: {user_input}</p>")
+        self.input_box.clear()
+        chatbot = ChatBot(user_input)
+        responce = chatbot.get_responce()
+        self.chat_area.append(
+            f"<p style='color:#333333; background-\
+            color:#E9E9E9'>Bot: {responce}</p>")
 
 
 app = QApplication(sys.argv)
